676d32648d0b0cd1424b2e17e7aa667544b36274
mild <boommild1@gmail.com>
2023-04-24 17:53:52 +0700

    เปลี่ยนตารางการทำความสะอาดมา cleaning comment

--- app/chambermaid.js
+++ app/chambermaid.js
@@ -5,6 +5,7 @@ module.exports = function(app, con, moment) {
             res.render(('maid/maidindex.ejs'), { room })
         })
     })
+
     app.post('/get_todo_data', (req, res) => {
         var { id } = req.body
         con.query("select * from cleaning_checklist", (err, checklist) => {
@@ -19,12 +20,27 @@ module.exports = function(app, con, moment) {
     app.post('/done_cleaning', (req, res) => {
         var { id, room_comment } = req.body
         const currentDate = moment().format('DD/MM/YYYY HH:mm:ss');
-        con.query("insert into room_comment values ('',?,?,?,'')", [room_comment, id, currentDate], (err, result) => {
+        con.query("select num_room from rooms where id = ?", [id], (err, num_room) => {
             if (err) throw err
-            con.query("update rooms set status = 2 where id = ?", [id], (err, result) => {
+            con.query("insert into cleaning_comment values ('',?,?,?)", [num_room[0].num_room, room_comment, currentDate], (err, result) => {
                 if (err) throw err
-                res.send({})
+                con.query("update rooms set status = 2 where id = ?", [id], (err, result) => {
+                    if (err) throw err
+                    res.send({})
+                })
             })
         })
     })
+
+    app.post('/maidroomchecking', (req, res) => {
+        con.query("select * from reserved where status = 2", (err, wait_for_check) => {
+            if (err) throw err
+            res.render('maid/maidroomchecking.ejs', { wait_for_check })
+        })
+    })
+
+    app.post('/done_checking', (req, res) => {
+        con.query("")
+    })
+
 }
--- app/checkout.js
+++ app/checkout.js
@@ -1,17 +1,26 @@
 module.exports = function(app, con, moment) {
     app.post('/checkout', (req, res) => {
-        con.query("SELECT *,reserved.id as reserv_id FROM reserved INNER JOIN customer ON reserved.cus_id = customer.id INNER JOIN roomstype ON reserved.id_typeroom = roomstype.id WHERE status = 1; ", (err, stay) => {
+        con.query("SELECT *,reserved.id as reserv_id FROM reserved INNER JOIN customer ON reserved.cus_id = customer.id INNER JOIN roomstype ON reserved.id_typeroom = roomstype.id WHERE status NOT IN (0, 4); ", (err, stay) => {
             if (err) throw err
             res.render(('checkout'), { stay })
         })
     })
-    44
+
+    app.post('/room_checking', (req, res) => {
+        var { id } = req.body
+        con.query("update reserved set status = 2 where id = ?", [id], (err, result) => {
+            if (err) throw err
+            res.send({});
+        })
+    })
+
+
     app.post('/confirm_checkout', (req, res) => {
         var { id, more_cus_comment, reserv_id } = req.body
         const currentDate = moment().format('DD/MM/YYYY HH:mm:ss');
         con.query("insert into cus_comment value ('',?,?,?)", [more_cus_comment, currentDate, id], (err, result) => {
             if (err) throw err
-            con.query("update reserved set status = 3 where id = ?", [reserv_id], (err, result) => {
+            con.query("update reserved set status = 4 where id = ?", [reserv_id], (err, result) => {
                 if (err) throw err
                 res.send({})
             })
